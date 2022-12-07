61a9e129192543ec894293bdf385a7f8f427c4b1
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-12-07 11:01:55 -0600

    starting products, still have strange bug to auto-populate edit inventory fields

---
+++ IMS/IMS.UseCases/Products/ViewProductsByNameUseCase.cs
@@ -0,0 +1,24 @@
+﻿using IMS.CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Products
+{
+    public class ViewProductsByNameUseCase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ViewProductsByNameUseCase(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product> ExecuteAsync(string name = "")
+        {
+            return await productRepository.GetProductsByNameAsync(name);
+        }
+    }
+}
